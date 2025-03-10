328808a7b368f93127daabcaaf159c8a846d3c9e
anhvietday <trandinhviet1403@gmail.com>
2025-03-10 17:54:02 +0700

    update

--- src/Layout/Header.js
+++ src/Layout/Header.js
@@ -8,7 +8,7 @@ function Headers() {
               <nav class="col-md-6 navbar navbar-expand-md navbar-dark">
                  <div class="container-fluid">
                     <a class="navbar-brand" href="https://example.com">
-                       <img src="/favicon.ico" alt="NetTruyen" width="30" height="30" class="d-inline-block align-text-top" />
+                       <img src="./favicon.ico" alt="NetTruyen" width="30" height="30" class="d-inline-block align-text-top" />
                        NetTruyen</a>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                        <span class="navbar-toggler-icon"></span>
